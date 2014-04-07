b46bd469ad6735c7b338d1368b2f43806e5b69e4
Jonathan Larkin <teckx5@gmail.com>
2014-04-07 09:03:45 +0100

    Code Clean Up

--- ParkAndSend/MYBlurIntroductionView/MYIntroductionPanel.m
+++ ParkAndSend/MYBlurIntroductionView/MYIntroductionPanel.m
@@ -135,13 +135,9 @@
     else {
         
 
+    panelTitleHeight = [self.PanelTitle sizeWithFont:kTitleFont constrainedToSize:CGSizeMake(frame.size.width - 2*kLeftRightMargins, MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping].height;
         
-        
-        
-        
-        // panelTitleHeight = [self.PanelTitle sizeWithFont:kTitleFont constrainedToSize:CGSizeMake(frame.size.width - 2*kLeftRightMargins, MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping].height;
-        
-     //   panelDescriptionHeight = [self.PanelDescription sizeWithFont:kDescriptionFont constrainedToSize:CGSizeMake(frame.size.width - 2*kLeftRightMargins, MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping].height;
+    panelDescriptionHeight = [self.PanelDescription sizeWithFont:kDescriptionFont constrainedToSize:CGSizeMake(frame.size.width - 2*kLeftRightMargins, MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping].height;
     }
     
     //Create title label
